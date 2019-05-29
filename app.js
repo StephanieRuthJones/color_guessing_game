ea1af23c5f105c9cf1cce4cfa78ddd2eafdd7990
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-05-29 15:14:44 -0600

    Added JS to assign colors to each square

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-var numSquares = 6
-var colors = []
-var pickedColor;
-var squares = document.querySelectorAll(".square")
-var resetButton = document.querySelector("#reset")
-var modeButtons = document.querySelectorAll(".mode")
+var numSquares = 6 //stoes number of squares available in gme per the mode
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(255, 225, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 255, 0)"
+] //contains random 6 or 3 RGB color code generated every time game is reset or mode is changed
+var pickedColor //color/option block selected by user
+var squares = document.querySelectorAll(".square") //array of squares on page
+var resetButton = document.querySelector("#reset") //new game button
+var modeButtons = document.querySelectorAll(".mode") //an array which has easy and hard mode buttons
 
+console.log('squares', squares)
+
+for (let i = 0; i < squares.length; i++) {
+
+    squares[i].style.backgroundColor = colors[i]
+
+}
